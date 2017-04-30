14b7d3d7f16fc70653f537eb260895013362b9fa
aamfahrurr <aam.fahrur@ti.stmikdb.ac.id>
2017-04-30 16:53:53 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -17,9 +17,8 @@ cd
 apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.831_all.deb"
 dpkg --install webmin_1.831_all.deb;
-apt-get -y -f install;
-rm /root/webmin_1.831_all.deb
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+rm /root/webmin_1.831_all.deb
 service webmin restart
 
 # install dropbear
